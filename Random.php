8a49c4616952af4da369abaed52d2b8cabc2678d
R2D2Alfa <arturo.ad2203@comunidad.unam.mx>
2021-05-02 20:22:09 -0500

    FEAT: barco de 3

--- Random.php
+++ Random.php
@@ -4,12 +4,23 @@
     $i=rand(1,10);
     $j=rand(1,10);
      if ($i<8) {
-        $barco4h =[ $i,$i+1,$i+2,$i+3,$i+4];
+        $barco4h =[ $i,$i+1,$i+2,$i+3];
      }
      elseif ($j<8) {
         $barco4v =[ $j,$j+1,$j+2,$j+3];
      } 
     } while ($i>8&&$j>8);
+    //barco de 3
+    do {
+        $i=rand(1,10);
+        $j=rand(1,10);
+         if ($i<9) {
+            $barco4h =[ $i,$i+1,$i+2];
+         }
+         elseif ($j<9) {
+            $barco4v =[ $j,$j+1,$j+2];
+         } 
+        } while ($i>9&&$j>9);
     
 
 
