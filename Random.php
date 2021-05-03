4f9ebd278a36351ce774b18b4310052b73539ed0
R2D2Alfa <arturo.ad2203@comunidad.unam.mx>
2021-05-02 22:11:11 -0500

    FEAT: barcos arreglados

--- Random.php
+++ Random.php
@@ -4,22 +4,38 @@
     $i=rand(1,10);
     $j=rand(1,10);
      if ($i<8) {
-        $barco4h =[ $i,$i+1,$i+2,$i+3];
-     }
-     elseif ($j<8) {
-        $barco4v =[ $j,$j+1,$j+2,$j+3];
+    //El barco se hace horizontal
+        $barco4h =[$i,$i+1,$i+2,$i+3];
+        if ($j) {
+         $barco4v = [$j];
+         $barco4 = array_merge($barco4h, $barco4v);
      } 
+     //El barco se hace vertical
+     elseif ($j<8) {
+        $barco4v =[$j,$j+1,$j+2,$j+3];
+        $barco4h = [$i];
+        $barco4 = array_merge($barco4h, $barco4v);
+     } 
+    }
+
+     
     } while ($i>8&&$j>8);
     //barco de 3
     do {
         $i=rand(1,10);
         $j=rand(1,10);
+        //El barco se hace horizontal
          if ($i<9) {
             $barco3h =[ $i,$i+1,$i+2];
-         }
-         elseif ($j<9) {
+            $barco3v = [$j]
+            $barco3 = array_merge($barco3h, $barco3v);
+        }
+        //El barco se hace vertical
+        elseif ($j<9) {
             $barco3v =[ $j,$j+1,$j+2];
-         } 
+            $barco3h =[$i];
+            $barco3 = array_merge($barco3h, $barco3v);
+        } 
         } while ($i>9&&$j>9);
     
 
