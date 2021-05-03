9bd86504af26c0cf249598f5ebc6f73ec6544fd6
R2D2Alfa <arturo.ad2203@comunidad.unam.mx>
2021-05-02 23:42:52 -0500

    FEAT: arreglos

--- Random.php
+++ Random.php
@@ -37,7 +37,22 @@
             $barco3 = array_merge($barco3h, $barco3v);
         } 
         } while ($i>9&&$j>9);
-    
-
+        
+        $EJEX = [
+        "@" => "0",
+        "A" => "1",
+        "B" => "2",
+        "C" => "3",
+        "D" => "4",
+        "E" => "5",
+        "F" => "6",
+        "G" => "7",
+        "H" => "8",
+        "I" => "9",
+        "J" => "10",
+        ];
+        foreach ($EJEX as $key => $value) {
+            $x[$value];
+        }
 
 ?>
